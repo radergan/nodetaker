f492850651a85bf03b143ec71b7dc523fd32134b
PJ Dalton <patrickjdalton1@gmail.com>
2020-12-17 00:27:03 -0600

    Adding heroku variable port logic and Procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const { json } = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
